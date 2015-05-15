4c0e12f9e9e36f8122807fd5b462b6a1fca0ad3e
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2015-05-15 17:31:25 +0300

    Filter by sex & skt.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
   def index
-    @main_children = Child.where(group: 'main').order('id asc')
-    @control_children = Child.where(group: 'control').order('id asc')
+
+    main_query = {}
+    control_query = {}
+    if params[:sex] && params[:sex] != 'all'
+      main_query[:sex] = params[:sex]
+      control_query[:sex] = params[:sex]
+    end
+    if params[:rozlad_skt] && params[:rozlad_skt] != 'all'
+      main_query[params[:rozlad_skt]] = true
+    end
+
+    @main_children = Child.where(main_query.merge({group: 'main'})).order('id asc')
+    @control_children = Child.where(control_query.merge({group: 'control'})).order('id asc')
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  match '/home', to: 'pages#index', via: [:get, :post]
 end
